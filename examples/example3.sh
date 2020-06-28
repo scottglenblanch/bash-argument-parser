24fbb68fde8d327c6ab74be015f50368070549e4
Scott Blanch <scottglenblanch@gmail.com>
2020-06-28 13:17:57 -0700

    improved example3.sh code

--- examples/example3.sh
+++ examples/example3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SCRIPT_URL="https://raw.githubusercontent.com/scottglenblanch/bash-argument-parser/main/src/arg-parser.sh"
+
 echo "Call this script (example3.sh) with parameters --person <some person> --place <some place> --thing <something>"
 
-source /dev/stdin <<<"$(curl https://raw.githubusercontent.com/scottglenblanch/bash-argument-parser/main/src/arg-parser.sh)" $@
+source /dev/stdin <<<"$(curl "${SCRIPT_URL}")" $@
 
 echo "--person is ${PERSON}"
 echo "--place is ${PLACE}"
